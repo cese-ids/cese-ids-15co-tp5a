c165487d2189df8b5510564d0ac3a69ca92e1832
Leandro Arrieta <leandroarrieta@gmail.com>
2021-07-12 20:26:46 -0300

    Se agregan los datos del alumno ARRIETA, Leandro

--- src/alumnos.c
+++ src/alumnos.c
@@ -46,4 +46,14 @@ bool SerializarAlumno(char * cadena, size_t espacio, const alumno_t alumno) {
     return (resultado >= 0);
 }
 
+bool LeandroArrieta(char * cadena, size_t espacio) {
+    const struct alumno_s alumno = {
+        .apellidos = "ARRIETA",
+        .nombres = "Leandro Ezequiel",
+        .documento = "29.747.571",
+    };
+
+    return SerializarAlumno(cadena, espacio, &alumno);
+}
+
 /*=====[Implementations of private functions]================================*/
