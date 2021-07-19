fa423a0e72f6c93f3a1d7e67483f493a534c3f22
fproko <fernandoprokopiuk@gmail.com>
2021-07-18 23:11:19 -0300

    Se agregan los datos del alumno PROKOPIUK, Fernando Andres

--- src/alumnos.c
+++ src/alumnos.c
@@ -46,4 +46,14 @@ bool SerializarAlumno(char * cadena, size_t espacio, const alumno_t alumno) {
     return (resultado >= 0);
 }
 
+bool FernandoProkopiuk(char * cadena, size_t espacio) {
+    const struct alumno_s alumno = {
+        .apellidos = "PROKOPIUK",
+        .nombres = "Fernando Andres",
+        .documento = "34.868.467",
+    };
+
+    return SerializarAlumno(cadena, espacio, &alumno);
+}
+
 /*=====[Implementations of private functions]================================*/
